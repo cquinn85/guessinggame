8372f346382b184057989de70c862ecc58c1a75f
cquinn85 <cequinn300@gmail.com>
2020-08-25 11:42:08 -0500

    guessing game pt1

---
+++ numberguessgame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace numberguessgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too Low");
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Too High");
+            }
+
+            else //this is a catch all no need for userInput == favNumber
+            {
+             Console.WriteLine($"Nevermind");
+            }
+
+           
+        
+        }
+    }
+}
+
+                      
+
+   
